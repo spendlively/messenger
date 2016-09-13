2762d035013c2da0c163a2aa09824000305a74c4
spendlively <spendlively@mail.ru>
2016-09-13 22:31:23 +0700

    Заливаю в продакшн node + httpd

---
+++ httpd/js/common.js
@@ -0,0 +1,64 @@
+$(document).ready(function(){
+
+	//Всплывающее меню для кнопки доступные сервисы
+	$('#rmenu-popover-btn').popover();
+
+	//Обработка клика по кнопке левого бокового меню
+	$('ul.lmenu>li').click(function(){
+
+		var targetId = $(this).attr('data-element-scroll-to'),
+			marginTop = parseInt($('#' + targetId).css('margin-top')),
+			topOffset = document.getElementById('top-navbar').getBoundingClientRect().bottom,
+			offset = $('#' + targetId).offset().top - topOffset - marginTop -20,
+			activeClsName = 'active';
+
+			$('ul.lmenu>li.' + activeClsName).removeClass(activeClsName);
+			$(this).addClass(activeClsName);
+
+		//Скролл на элемент
+		$('html, body').animate({
+            scrollTop: offset +  'px'
+        }, 'fast');
+
+
+	});
+
+	var notifBtnCounter = 0;
+	$('#header-top-notification-btn').click(function(){
+
+		var text = 'Уведомления включены!';
+		if(++notifBtnCounter%2){
+			text = 'Уведомления отлючены!';
+		}
+
+		$('.top-right').notify({
+	    	message: { text: text },
+	    	type: 'info',
+	  	}).show();
+	});
+
+
+	//Switcher кнопочки
+	$('input#switch-modal').bootstrapSwitch('state', true, true);
+
+	//Popover верхнего меню вкл/выкл сервисов
+	var switcherFlag = false;
+	$("[data-toggle=popover]").popover({
+		html: true, 
+		container: 'body',
+		content: function() {
+
+			//Поскольку bootstrapSwitch() не работает если элемент скрыт
+			//выполняем bootstrapSwitch() после открытия popover
+			if(!switcherFlag){
+				switcherFlag = true;
+				setTimeout(function(){
+					$('input#popover-switch-service').bootstrapSwitch('state', true, true);
+				}, 101);
+
+			}
+        	return $('#popover-content').html();
+        }
+	});
+
+});
---
+++ main.js
@@ -0,0 +1,72 @@
+const electron = require('electron');
+const app = electron.app;  // Модуль контролирующей жизненный цикл нашего приложения.
+const BrowserWindow = electron.BrowserWindow;  // Модуль создающий браузерное окно.
+const fs = require('fs');
+const ipcMain = require('electron').ipcMain;
+
+//Чтение конфига
+var configEncoded = fs.readFileSync('data/config.json', 'utf8');
+var configText = decodeURIComponent(configEncoded);
+var config = JSON.parse(configText);
+// console.log(content);
+
+//Расшариваю конфиг
+global.config = config;
+
+//Сохранение конфига
+ipcMain.on('save-config', function(event) {
+    config = global.config;
+    configText = JSON.stringify(config);
+    fs.writeFileSync('data/config.json', configText);
+});
+
+// Опционально возможность отправки отчета о ошибках на сервер проекта Electron.
+//electron.crashReporter.start();
+electron.crashReporter.start({
+    productName: 'YourName',
+    companyName: 'YourCompany',
+    submitURL: 'https://your-domain.com/url-to-submit',
+    autoSubmit: true
+})
+
+// Определение глобальной ссылки , если мы не определим, окно
+// окно будет закрыто автоматически когда JavaScript объект будет очищен сборщиком мусора.
+var mainWindow = null;
+
+// Проверяем что все окна закрыты и закрываем приложение.
+app.on('window-all-closed', function() {
+    // В OS X обычное поведение приложений и их menu bar
+    //  оставаться активными до тех пор пока пользователь закроет их явно комбинацией клавиш Cmd + Q
+    if (process.platform != 'darwin') {
+        app.quit();
+    }
+});
+
+// Этот метод будет вызван когда Electron закончит инициализацию 
+// и будет готов к созданию браузерных окон.
+app.on('ready', function() {
+    // Создаем окно браузера.
+    mainWindow = new BrowserWindow({
+        width: 800, 
+        height: 580,
+        minWidth: 600,
+        minHeight: 480
+        // 'min-width': 300,
+        // 'min-height': 300,
+    });
+
+    // и загружаем файл index.html нашего веб приложения.
+   mainWindow.loadURL('file://' + __dirname + '/httpd/index.html');
+    // mainWindow.loadURL('http://127.0.0.1:8888/index.html');
+
+    // Открываем DevTools.
+   mainWindow.webContents.openDevTools();
+
+    // Этот метод будет выполнен когда генерируется событие закрытия окна.
+    mainWindow.on('closed', function() {
+        // Удаляем ссылку на окно, если ваше приложение будет поддерживать несколько
+        // окон вы будете хранить их в массиве, это время 
+        // когда нужно удалить соответствующий элемент.
+        mainWindow = null;
+    });
+});
