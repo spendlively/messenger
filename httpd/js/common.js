df0fb7fabef2700485837a8a04abb767685f3c6e
spendlively <spendlively@mail.ru>
2016-10-03 00:05:27 +0700

    аа-а-а

--- httpd/js/common.js
+++ httpd/js/common.js
@@ -10,3 +10,28 @@ setTimeout(function(){
 
 }, 1000);
 });
+
+
+window.a = function(text){
+console.log(1)	
+  if (!("Notification" in window)) {
+console.log(2)	
+    alert("This browser does not support system notifications");
+  }
+
+  else if (Notification.permission === "granted") {
+console.log(3)	
+    var notification = new Notification("Hi there!");
+    console.log(notification);
+  }
+
+  else if (Notification.permission !== 'denied') {
+console.log(4)	
+    Notification.requestPermission(function (permission) {
+      if (permission === "granted") {
+        var notification = new Notification("Hi there!");
+      }
+    });
+  }
+console.log(5)	
+};
